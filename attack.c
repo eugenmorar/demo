8a006e0fa854636b835dc5c8e8c9903132c840d7
root <root@ip-172-31-33-101.us-west-2.compute.internal>
2019-11-22 16:52:28 +0000

    delete

--- attack.c
+++ attack.c
@@ -9,6 +9,6 @@ static void malicious() __attribute__((constructor));
 
 void malicious() {
 //    system("touch /tmp/owned");
-    system("/usr/local/bin/score d8500581-b77c-4f2e-813a-77eda1a9c6e6"); 
+    system("/usr/local/bin/score d8500581-b77c-4f2e-813a-77eda1a9c6e6");
 //    system("sleep 20");
 }
